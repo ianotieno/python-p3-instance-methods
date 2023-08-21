d9ca04ba70cc62c93e19ff1fd359b0eea30f354e
ianotieno <ianotieno23@gmail.com>
2023-08-21 11:43:04 +0300

    second test

--- lib/dog.py
+++ lib/dog.py
@@ -2,9 +2,11 @@
 class Dog:
     def bark(self):
         print("Woof!")
+    def sit(self):
+        print ("The dog is sitting")
 
 fido = Dog()
 fido.bark()
 # Woof!
 snoopy = Dog()
-snoopy.bark()
+snoopy.sit()
